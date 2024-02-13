ee94c8c391f57d8cbdaf2ee8f008c84992a358f4
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-02-13 12:40:59 +0100

    Use new SQL user account

--- student/conn/database.php
+++ student/conn/database.php
@@ -1,8 +1,8 @@
 <?php
 define('HOST', 'localhost');
 define('DATABASE', 'student');
-define('USER', 'tim');
-define('PASSWORD', 'tim');
+define('USER', 'localview');
+define('PASSWORD', 'dblocalview');
 
 try {
     $dbconn = new PDO("mysql:host=" . HOST . ";dbname=" . DATABASE . ";charset=utf8mb4", USER, PASSWORD);
