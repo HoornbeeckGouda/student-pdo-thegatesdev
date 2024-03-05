a67b370d6d7345bee480387e8f2bf0a7eb1355d2
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-03-05 11:57:30 +0100

    Make header title optional

--- student/parts/header.php
+++ student/parts/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>
-        <?php echo $header_title ?>
+        <?php echo $header_title ?? "Page" ?>
     </title>
     <link rel="stylesheet" type="text/css" href="css/base.css">
     <link rel="stylesheet" type="text/css" href="css/student.css">
