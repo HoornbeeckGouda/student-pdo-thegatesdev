8530bc9cc67cba46dafea367c8963ea653456417
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2024-03-06 11:55:10 +0100

    Move HTML generation out of student connection class

--- student/conn/StudentConnection.php
+++ student/conn/StudentConnection.php
@@ -34,38 +34,6 @@ class StudentConnection
         return $statement->fetchAll(PDO::FETCH_BOTH);
     }
 
-    public function getFullStudentHTMLTable(): string
-    {
-        $building_table = "<table id='students'>
-        <tr>
-            <th>studentnr</th>
-            <th>voornaam</th>
-            <th>tussenvoegsel</th>
-            <th>achternaam</th>
-            <th>straat</th>
-            <th>postcode</th>
-            <th>woonplaats</th>
-            <th>email</th>
-            <th>klas</th>
-            <th>geboortedatum</th>
-        </tr>";
-        foreach ($this->getFullStudentArray() as $row) {
-            $building_table .= "<tr>
-                            <td>" . $row['id'] . "</td>
-                            <td>" . $row['voornaam'] . "</td>
-                            <td>" . $row['tussenvoegsel'] . "</td>
-                            <td>" . $row['achternaam'] . "</td>
-                            <td>" . $row['straat'] . "</td>
-                            <td>" . $row['postcode'] . "</td>
-                            <td>" . $row['woonplaats'] . "</td>
-                            <td>" . $row['email'] . "</td>
-                            <td>" . $row['klas'] . "</td>
-                            <td>" . $row['geboortedatum'] . "</td>
-                        </tr>";
-        }
-        return "$building_table</table>";
-    }
-
     public function changeStudent(int $student_id, $voornaam, $tussenvoegsel, $achternaam, $straat, $postcode, $woonplaats, $email, $klas, $geboortedatum): bool
     {
         $statement = $this->pdo->prepare("UPDATE $this->student_table_name SET 
--- student/studenten.php
+++ student/studenten.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION["user"])){
+if (!isset($_SESSION["user"])) {
     header("Location:index.php");
     exit;
 }
@@ -11,6 +11,33 @@ include "conn/database.php";
 include "conn/StudentConnection.php";
 
 $studentConn = new StudentConnection($dbconn);
-echo $studentConn->getFullStudentHTMLTable();
+$building_table = "<table id='students'>
+        <tr>
+            <th>studentnr</th>
+            <th>voornaam</th>
+            <th>tussenvoegsel</th>
+            <th>achternaam</th>
+            <th>straat</th>
+            <th>postcode</th>
+            <th>woonplaats</th>
+            <th>email</th>
+            <th>klas</th>
+            <th>geboortedatum</th>
+        </tr>";
+foreach ($studentConn->getFullStudentArray() as $row) {
+    $building_table .= "<tr>
+                            <td>" . $row['id'] . "</td>
+                            <td>" . $row['voornaam'] . "</td>
+                            <td>" . $row['tussenvoegsel'] . "</td>
+                            <td>" . $row['achternaam'] . "</td>
+                            <td>" . $row['straat'] . "</td>
+                            <td>" . $row['postcode'] . "</td>
+                            <td>" . $row['woonplaats'] . "</td>
+                            <td>" . $row['email'] . "</td>
+                            <td>" . $row['klas'] . "</td>
+                            <td>" . $row['geboortedatum'] . "</td>
+                        </tr>";
+}
+echo "$building_table</table>";
 
 include "parts/footer.php";
